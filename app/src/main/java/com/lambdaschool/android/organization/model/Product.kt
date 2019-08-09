4a7c286618a10257631b05e335a9c77aa0639f5e
Janiece Thomas <janiece.t90@gmail.com>
2019-08-09 00:52:24 -0400

    Created Product Class

---
+++ app/src/main/java/com/lambdaschool/android/organization/model/Product.kt
@@ -0,0 +1,12 @@
+package com.lambdaschool.android.organization.model
+
+class Product(
+
+    productName: String = "Shampoo"
+    productDescription: String = "Naturally conditins the scapl"
+    productPrice: Double = 7.99
+    productTax: Double = productPrice * .075
+    productDiscount: Double = .50
+
+
+)
